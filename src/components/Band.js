2ec122b72e2b92456bef248979fe58f191c710ab
John Ratana <jratana1@gmail.com>
2021-03-11 19:30:41 -0500

    commit tree b2a5f90f69450ebffe4b0f956d71b0c3e9e55fd5

--- src/components/Band.js
+++ src/components/Band.js
@@ -1,13 +1,35 @@
+// import React, { Component } from 'react';
+
+// class Band extends Component {
+
+//   render() {
+//     return(
+//       <div>
+//         <span>{this.props.band}</span><button onClick={() => this.props.delete(this.props.key)}>DELETE</button>
+//       </div>
+//     );
+//   }
+// };
+
+// export default Band;
+
 import React, { Component } from 'react';
 
 class Band extends Component {
 
+  handleOnClick = () => {
+    this.props.deleteBand(this.props.id)
+  }
+
   render() {
     return(
       <div>
-        Band Component
+        <li>{this.props.name}</li>
+        <button onClick={this.handleOnClick}>
+          Delete
+        </button>
       </div>
-    );
+    )
   }
 };
 
